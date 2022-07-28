56771f2242ff10381a9a223c05e6a1bd76311ec5
Frank Calise <fcalise@gmail.com>
2022-07-28 00:39:46 -0700

    New Ignite 7.14.1 app

---
+++ index.js
@@ -0,0 +1,9 @@
+// This is the first file that ReactNative will run when it starts up.
+// If you use Expo (`yarn expo:start`), the entry point is ./App.js instead.
+// Both do essentially the same thing.
+
+import App from "./app/app.tsx"
+import { AppRegistry } from "react-native"
+
+AppRegistry.registerComponent("IgniteUni6", () => App)
+export default App
